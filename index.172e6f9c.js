1a7e04e54a637bdba5c702a6743c52e7ab3fcc72
wielembe <49246897+wielembe@users.noreply.github.com>
2023-07-06 23:45:03 +0000

    Deploying to gh-pages from @ wielembe/Ice@8ceb31d98160f731c489cb9428a03a9c9f478d23 🚀

---
+++ index.172e6f9c.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-pagination",prevEl:".swiper-pagination"}});
+//# sourceMappingURL=index.172e6f9c.js.map
--- index.74a9c2da.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".review__pagination"},navigation:{nextEl:".review__pagination",prevEl:".review__pagination"}});
-//# sourceMappingURL=index.74a9c2da.js.map
