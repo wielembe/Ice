af84141e780a36e68afae5dce62805c180082cb8
wielembe <49246897+wielembe@users.noreply.github.com>
2023-07-07 00:47:50 +0000

    Deploying to gh-pages from @ wielembe/Ice@f1af21f25b5879f43f2fd76b5d15d46717a67601 🚀

---
+++ index.1eedc06a.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-pagination",prevEl:".swiper-pagination"}});
+//# sourceMappingURL=index.1eedc06a.js.map
