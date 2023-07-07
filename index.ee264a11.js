89535c7c2ba54c58b90dc0f8058301677b199017
wielembe <49246897+wielembe@users.noreply.github.com>
2023-07-07 16:39:38 +0000

    Deploying to gh-pages from @ wielembe/Ice@ec06b181db10cb9f5cc6f8e74cfdd3a72a6237dd 🚀

---
+++ index.ee264a11.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[about-data-modal-open]"),closeModalBtn:document.querySelector("[about-data-modal-close]"),modal:document.querySelector("[about-data-modal]")};function e(){o.modal.classList.toggle("about-is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.ee264a11.js.map
